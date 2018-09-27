db6fdbb5d163a2f640149d1b4466b388a0241524
oscardoudou <oscar9591@icloud.com>
2018-09-27 17:28:59 -0400

    add task_struct in thread and exe_tid in container_node and finish find_thread(actually find next thread_node) and switch

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -49,10 +49,13 @@ static DEFINE_MUTEX(access);
 
 typedef struct container_node container_node;
 typedef struct thread_node thread_node;
-
+typedef struct task_struct task_struct;
 struct thread_node{
 	int tid;
-	thread_node* next;
+	//not sure if cid in thread_node is necessary, could get exeuting_tid from global head
+	int cid;
+	thread_node* next;	
+	task_struct* thread_block;
 };
 
 struct container_node{
@@ -61,6 +64,7 @@ struct container_node{
 	thread_node* head_thread;
 	thread_node* tail_thread;
 	int num_of_thread;
+	int exe_tid;
 };
 
 //can't kmalloc in file-level scope
@@ -76,6 +80,7 @@ int create_container(int cid){
 	new_node->head_thread = NULL;
 	new_node->tail_thread = NULL;
 	new_node->num_of_thread = 0;
+	new_node->exe_tid = -1;
 	if (head == NULL)
 	{
 		head = new_node;
@@ -156,9 +161,40 @@ container_node* find_container(int cid){
 	return NULL;
 }
 
+thread_node* find_thread(int tid){
+	container_node* curr = head;
+	//thread_node* curr_thread = NULL;
+	if(curr == NULL){
+		printk("container list is empty\n");
+		return NULL;
+	}
+	while(curr != tail){
+		if(curr->exe_tid == tid){
+			printk("find current executing thread %d in container %d\n", curr->exe_tid, curr->cid);
+			curr->head_thread = curr->head_thread->next;
+			curr->exe_tid = curr->head_thread->tid;
+			curr->tail_thread = curr->tail_thread->next;
+			return curr->head_thread; 	
+		}
+		curr = curr->next;
+	}
+	if(curr->exe_tid == tid ){
+		printk("find current executing thread %d in container %d\n",curr->exe_tid, curr->cid);
+		curr->head_thread = curr->head_thread->next;
+		curr->exe_tid = curr->head_thread->tid;
+		curr->tail_thread = curr->tail_thread->next;
+		return curr->head_thread;
+	}
+	printk("curr->exe_tid: %d",curr->exe_tid);
+	printk("thread %d doestn't find in any containers\n",tid);
+	return NULL;
+}
+
 int bind_thread(container_node* container, int tid){
 	thread_node* new_thread = (thread_node*)kmalloc(sizeof(thread_node),GFP_KERNEL);
 	new_thread->tid = tid;
+	new_thread->cid = container->cid;
+	new_thread->thread_block = current;
 	//printk("kmalloc for new thread_node success\n");
 	// thread_node* head = container->head_thread;
 	// thread_node* tail = container->tail_thread;
@@ -168,6 +204,7 @@ int bind_thread(container_node* container, int tid){
 		// head = new_thread;
 		// head->next = head;
 		// tail = head;
+		container->exe_tid = tid;
 		container->head_thread = new_thread;
 		container->head_thread->next = container->head_thread;
 		container->tail_thread = container->head_thread;
@@ -221,7 +258,8 @@ int unbind_thread(container_node* container, int tid){
 	if(container->head_thread->tid == tid && container->tail_thread->tid == tid){
 		container->head_thread = NULL;
 		container->tail_thread = NULL;
-		printk("unbind last thread(task) %d in container %d\n",tid,container->cid);
+		container->exe_tid = -1;
+		printk("unbind last thread(task) %d in container %d\n",tid, container->cid);
 		container->num_of_thread--;
 		return 0;
 	}
@@ -229,11 +267,14 @@ int unbind_thread(container_node* container, int tid){
 		prev = curr;
 		curr = curr->next;	
 	}
+	//tail pointer dont need to change
 	if(curr == container->head_thread)
-		container->head_thread = curr->next; 
+		container->head_thread = curr->next;
+	//head pointer dont need to change 
 	if(curr->next == container->head_thread)
 		container->tail_thread = prev;
 	prev->next = curr->next;
+	container->exe_tid = curr->next->tid;
 	container->num_of_thread--;
 	printk("delete thread %d in container %d\n", curr->tid,container->cid);
 	return 0;
@@ -293,23 +334,34 @@ int print_container_list(void){
 int processor_container_delete(struct processor_container_cmd __user *user_cmd)
 {
 	struct processor_container_cmd cmd;
-	struct task_struct* task = current;
+	task_struct* task = current;
 	int tid = task->pid;
 	copy_from_user(&cmd, user_cmd, sizeof(cmd));
-	printk("entering lock\n");
+	printk("entering delete lock\n");
 	mutex_lock(&access);
 	container_node* delete_node = find_container(cmd.cid);
 	//mutex_unlock(&access);
-	printk("inside lock\n");
+	printk("inside delete lock\n");
 	printk("Deleting a container ");
 	printk("%llu\n",cmd.cid);
 	//currently assume we only create one task for each container
 	//mutex_lock(&access);
-	unbind_thread(delete_node,tid);
-	if(delete_node->num_of_thread == 0)
+	//this one 
+	//unbind_thread(delete_node,tid);
+	//attention there are two var here one is deleted_node(thread) the othere is delete_node(container)
+	if(delete_node->num_of_thread == 1){
+		printk("only one task left\n");
+		unbind_thread(delete_node,tid);
 		delete_container(cmd.cid);
+	}
+	else{
+		printk("more than 1 task in container,actually %d \n",delete_node->num_of_thread);
+		thread_node* deleted_node= find_thread(tid);
+		unbind_thread(delete_node,tid);
+		wake_up_process(deleted_node->thread_block);
+	}
 	mutex_unlock(&access);
-	printk("just exit lock\n");
+	printk("just exit delete lock\n");
     return 0;
 }
 
@@ -323,9 +375,8 @@ int processor_container_delete(struct processor_container_cmd __user *user_cmd)
  */
 int processor_container_create(struct processor_container_cmd __user *user_cmd)
 {	struct processor_container_cmd cmd;
-	struct task_struct* task = current;
+	task_struct* task = current;
 	int tid = task->pid;
-	
 	// printk("whose cid is  %llu\n",user_cmd->cid);
 	// int cid = (int)user_cmd->cid;
 	//cause every time a threadbody create would call this function
@@ -333,21 +384,26 @@ int processor_container_create(struct processor_container_cmd __user *user_cmd)
 	printk("Creating a container ");
 	copy_from_user(&cmd, user_cmd, sizeof(cmd));
 	printk("%llu\n",cmd.cid);
-	printk("entering lock\n");
+	printk("entering create lock\n");
 	mutex_lock(&access);
-	printk("inside lock\n");
+	printk("inside create lock\n");
 	if(find_container(cmd.cid) == NULL){
+		printk("container %llu doesn't exist\n",cmd.cid);
 		create_container(cmd.cid);
 		//printk("this line should be seen cause it's before calling find_container");
 		container_node* new_node = find_container(cmd.cid);
 		bind_thread(new_node,tid);
+		mutex_unlock(&access);
 	}
 	else{
 		container_node* exist_node = find_container(cmd.cid);
 		bind_thread(exist_node,tid);
+		printk("Thread %d is going to sleep\n", tid);
+		set_current_state(TASK_INTERRUPTIBLE);
+		mutex_unlock(&access);
+		schedule();
 	}
-	mutex_unlock(&access);
-	printk("just exit lock\n");
+	printk("just exit create lock\n");
     return 0;
 }
 
@@ -359,7 +415,28 @@ int processor_container_create(struct processor_container_cmd __user *user_cmd)
  */
 int processor_container_switch(struct processor_container_cmd __user *user_cmd)
 {	
-	printk("Switching between container\n");
+	task_struct* task = current;
+	int tid = task->pid;
+
+	printk("Switch a container\n");
+	printk("enter switch lock\n");
+	mutex_lock(&access);
+	thread_node* exe_thread_node_next = find_thread(tid);
+	if(exe_thread_node_next != NULL){
+		printk("exe_thread_node_next is not NULL\n");
+		mutex_unlock(&access);
+		printk("exit switch lock\n");
+		set_current_state(TASK_INTERRUPTIBLE);
+		wake_up_process(exe_thread_node_next->thread_block);
+		printk("curr thread %d,next schedule thread %d\n",tid,exe_thread_node_next->thread_block->pid);
+		schedule();
+	}
+	else{
+		printk("exe_thread_node_next we get from get_thread() is NULL\n");
+		mutex_unlock(&access);
+		printk("exit switch lock\n");
+	}
+	printk("Finish switching container\n");
     return 0;
 }
 
